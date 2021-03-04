59853afc8ec5839fe34b7d17b6cf95f459bb2ba8
Daniel Richter <daniel.richter@hpi.de>
2021-03-04 18:21:27 +0100

    German localization

--- l10n/de.js
+++ l10n/de.js
@@ -18,7 +18,7 @@ OC.L10N.register(
     "Allow login only for specified organizations": "Nur für Mitglieder der Organisationen",
 
     "Settings for social login successfully saved": "Die Einstellungen für Social Login wurden erfolgreich gespeichert",
-    "Do you realy want to remove {providerTitle} provider ?": "Möchten Sie den Anbieter {providerTitle} wirklich entfernen?",
+    "Do you realy want to remove {providerTitle} provider ?": "Möchten Sie den Autorisierungsdienst {providerTitle} wirklich entfernen?",
     "Some error occurred while saving settings": "Es trat ein Fehler beim Speichern der Einstellungen auf",
     "Confirm remove": "Entfernen bestätigen"
   },
